3940d047e5efe4facc84236ae09084b73d282e4a
Edin Cenanovic <cenanovice@gmail.com>
2016-10-15 21:38:26 +0200

    Added nginx installation and configuration

--- Vagrantfile
+++ Vagrantfile
@@ -3,5 +3,8 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
-  config.vm.provision = "shell", path: "provision.sh"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.provision "shell" do |s|
+    s.path = "provision.sh"
+  end
 end
--- provision.sh
+++ provision.sh
@@ -1,8 +1,29 @@
-!#!/usr/bin/env bash
+#!/bin/bash
 
 ########################
-# Installing nginx
+# Jenkins
 ########################
-sudo apt-get update
-sudo apt-get -y install nginx
+echo "Installing Jenkins"
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install jenkins > /dev/null 2>&1
+
+########################
+# nginx
+########################
+echo "Installing nginx"
+sudo apt-get -y install nginx > /dev/null 2>&1
 sudo service nginx start
+
+########################
+# Configuring nginx
+########################
+echo "Configuring nginx"
+cd /etc/nginx/sites-available
+sudo rm default ../sites-enabled/default
+sudo cp /vagrant/VirtualHost/jenkins /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
+sudo service nginx restart
+sudo service jenkins restart
+echo "Success"
