33be84c88f935d942ba9bf15056d8b1e95cc6f66
Maksim Luzik <maksim.luzik@gmail.com>
2017-09-29 13:55:54 +0300

    update docker version

--- provision.sh
+++ provision.sh
@@ -24,7 +24,10 @@ sudo apt-get -y install npm
 # Docker
 ########################
 echo "Installing Docker"
-sudo apt-get -y install docker.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
 sudo systemctl enable docker
 sudo usermod -aG docker ${USER}
 sudo usermod -aG docker jenkins
