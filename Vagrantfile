eb3898b8d17e9a8738159a41b54b132bada38145
Maksim Luzik <maksim.luzik@gmail.com>
2017-09-29 14:29:38 +0300

    add shared folder

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,7 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.synced_folder ".", "host_machine"
   config.vm.provider :virtualbox do |vb|
       vb.name = "jenkins"
   end
--- provision.sh
+++ provision.sh
@@ -8,8 +8,8 @@ sudo apt-get -y install openjdk-8-jre-headless
 echo "Installing Jenkins"
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update > /dev/null 2>&1
-sudo apt-get -y install jenkins > /dev/null 2>&1
+sudo apt-get update #> /dev/null 2>&1
+sudo apt-get -y install jenkins #> /dev/null 2>&1
 
 
 ########################
